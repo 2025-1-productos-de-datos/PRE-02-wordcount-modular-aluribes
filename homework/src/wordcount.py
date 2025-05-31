53dad63e51a8cc9778e3cf542e532317740729ad
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2025-05-31 00:32:48 -0500

    Clase mayo 4 updated

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,38 +2,30 @@
 # Ctrl Shift L
 # Ctrl Alt Flecha arriba/abajo
 
+import sys
 
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.split_into_words import split_into_words
 from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
-from ._internals.write_word_counts import write_word_counts
 
 def main():
 
-    # Read
-    all_lines = read_all_lines()
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        sys.exit(1)
 
-    # Preprocess
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
-
-    # Split
     words = split_into_words(all_lines)
-
-    # Count
     counter = count_words(words)
+    write_word_counts(counter, output_folder)
 
-    # count the frequency of the words in the files in the input directory
-    # counter={}
-    # for filename in input_file_list:
-    #     with open('data/input/'+filename) as f:
-    #         for l in f:
-    #             for w in l.split( ):
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    write_word_counts(counter)
 
 if __name__ == "__main__":
     main()
